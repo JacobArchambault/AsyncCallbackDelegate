6740cb76616eddf91a24ab8b7549e8e5f22f72fd
Jacob Archambault <jacob.archambault@waystar.com>
2019-10-14 12:41:55 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncCallbackDelegate
+{
+    public delegate int BinaryOp(int x, int y);
+
+    class Program
+    {
+        private static bool isDone = false;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** AsyncCallback Delegate Example *****");
+
+            // Print out the ID of the executing thread.
+            Console.WriteLine("Main() invoked on thread {0}.", Thread.CurrentThread.ManagedThreadId);
+
+            // Invoke Add() on a secondary thread.
+            BinaryOp b = new BinaryOp(Add);
+            IAsyncResult ar = b.BeginInvoke(10, 10, new AsyncCallback(AddComplete), null);
+
+            // This message will keep printing until the Add() method is finished.
+            while (!isDone)
+            {
+                Console.WriteLine("Working...");
+                Thread.Sleep(1000);
+            }
+
+            Console.ReadLine();
+
+        }
+
+        static int Add(int x, int y)
+        {
+            // Print out the ID of the executing thread.
+            Console.WriteLine("Add() invoked on thread {0}.", Thread.CurrentThread.ManagedThreadId);
+
+            // Pause to simulate a lengthy operation.
+            Thread.Sleep(5000);
+            return x + y;
+        }
+
+        static void AddComplete(IAsyncResult iar)
+        {
+            Console.WriteLine("AddComplete() invoked on thread {0}.", Thread.CurrentThread.ManagedThreadId);
+            Console.WriteLine("Your addition is complete");
+            isDone = true;
+        }
+    }
+}
